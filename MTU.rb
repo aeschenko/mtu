ad3c03c2a053bf63825345030951184fd938333d
Alexander Eschenko <nonpafos@gmail.com>
2016-07-20 16:20:39 +0300

    target new repo

--- MTU.rb
+++ MTU.rb
@@ -3,7 +3,7 @@
 
 class Mtu < Formula
     homepage "http://confluence.tango.corp/pages/viewpage.action?pageId=5716692"
-    url "https://github.tango.me/mfilkov/mtu.git"
+    url "https://github.tango.me/client-qa/mtu.git"
     version "3.1.1"
 
     depends_on 'bash'
